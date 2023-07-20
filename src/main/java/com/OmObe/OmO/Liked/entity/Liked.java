cacdb74b5d9281e31e6bf4c17d290e6ad63eb05b
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-07-20 17:21:57 +0900

    feat Liked 엔티티 추가

---
+++ src/main/java/com/OmObe/OmO/Liked/entity/Liked.java
@@ -0,0 +1,25 @@
+package com.OmObe.OmO.Liked.entity;
+
+import com.OmObe.OmO.Board.entity.Board;
+import com.OmObe.OmO.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Liked {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long likedId;
+
+    @Column
+    private Member member;
+
+    @Column
+    private Board board;
+}
