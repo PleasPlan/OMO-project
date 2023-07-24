1a92ab5f0a100365cd649096896573d286430b3c
seunghyun711 <seunghyun711@gmail.com>
2023-07-24 11:12:50 +0900

    [be/feat] : ExceptionCode 추가

---
+++ src/main/java/com/OmObe/OmO/exception/ExceptionCode.java
@@ -0,0 +1,21 @@
+package com.OmObe.OmO.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found. Please check the member ID and try again."),
+    EMAIL_ALREADY_EXIST(404, "This Email is already exist. Please check the email"),
+    NICKNAME_ALREADY_EXIST(404, "This Nickname is already exist. Please check the nickname");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
+
