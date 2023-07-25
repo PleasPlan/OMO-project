d63bbb282445e2ebab1a826f2a6e539a4efbf317
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-07-25 20:04:51 +0900

    fix CommentDto 애너테이션 추가

--- src/main/java/com/OmObe/OmO/Comment/dto/CommentDto.java
+++ src/main/java/com/OmObe/OmO/Comment/dto/CommentDto.java
@@ -1,16 +1,26 @@
 package com.OmObe.OmO.Comment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public class CommentDto {
 
+    @Getter
+    @Setter
+    @AllArgsConstructor
     public static class Post{
         private long BoardId;
         @NotBlank(message = "내용을 입력해주세요.")
         private String content;
     }
 
+    @Getter
+    @Setter
     public static class Patch{
         private long commentId;
         private String content;
