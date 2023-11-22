fa4cee60dbd650e0cbe2ad65885c4e52b1430b6a
seunghyun711 <seunghyun711@gmail.com>
2023-11-23 00:08:15 +0900

    [be/fix] : 테스트 코드 주석 처리

--- src/test/java/com/OmObe/OmO/OmOApplicationTests.java
+++ src/test/java/com/OmObe/OmO/OmOApplicationTests.java
@@ -3,11 +3,11 @@ package com.OmObe.OmO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OmOApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class OmOApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
