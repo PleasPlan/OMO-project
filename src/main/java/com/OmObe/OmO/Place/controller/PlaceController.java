d71e2acf1597d43cd2ac447b195a23dcee34d6aa
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-12-01 15:34:15 +0900

    feat : 장소 리스트 및 좋아요&찜 설정완료

--- src/main/java/com/OmObe/OmO/Place/controller/PlaceController.java
+++ src/main/java/com/OmObe/OmO/Place/controller/PlaceController.java
@@ -1,7 +1,10 @@
 package com.OmObe.OmO.Place.controller;
 
 import com.OmObe.OmO.Place.service.PlaceService;
+import com.OmObe.OmO.util.PairJ;
+import com.nimbusds.jose.util.Pair;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,7 +24,20 @@ public class PlaceController {
 
     @GetMapping("/{category}")
     public ResponseEntity getPlaces(@PathVariable("category") String category){
-        String response = placeService.getPlaces(category);
+        PairJ<Double, Double> middle = new PairJ<>();
+        middle.setFirst(37.514322572335935);    // y (위도)
+        middle.setSecond(127.06283102249932);   // x (경도)
+
+        String response = placeService.getPlaces(category, middle);
+        // json 응답 중 "is_end":false 가 되어있다면 다음 페이지가 존재하는 것이다.
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/{placeId}")
+    public ResponseEntity MineOrRecommend(@PathVariable("placeId") long placeId,
+                                          @RequestParam @Range(min = -1L,max = 1L) long mine,
+                                          @RequestParam @Range(min = -1L,max = 1L) long recommend){
+        String response = placeService.putMineOrRecommend(placeId,mine, recommend);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/OmObe/OmO/Place/service/PlaceService.java
+++ src/main/java/com/OmObe/OmO/Place/service/PlaceService.java
@@ -1,5 +1,11 @@
 package com.OmObe.OmO.Place.service;
 
+import com.OmObe.OmO.Place.entity.Place;
+import com.OmObe.OmO.Place.repository.PlaceRepository;
+import com.OmObe.OmO.util.PairJ;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,17 +17,26 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class PlaceService {
 
-    @Value("${naver-map.id}")
-    private String id;
-    @Value("${naver-map.pw}")
-    private String pw;
+//    @Value("${naver-map.id}")
+//    private String id;
+//    @Value("${naver-map.pw}")
+//    private String pw;
+    @Value("${kakao-map.key}")
+    private String key;
 
-    public String getPlaces(String category) {
+    private final PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public String getPlaces(String category, PairJ<Double, Double> middle) {
         String keyword;
         try{
             keyword = URLEncoder.encode(category, "UTF-8");
@@ -29,16 +44,38 @@ public class PlaceService {
             throw new RuntimeException("Encoding Failed",e);
         }
 
-        String webAddress = "https://openapi.naver.com/v1/search/local.json?query="+keyword+"&display=10&start=1&sort=random";
+        String y = middle.getFirst().toString();
+        String x = middle.getSecond().toString();
+
+        String webAddress = "https://dapi.kakao.com/v2/local/search/keyword.json?y="+y+"&x="+x+"&page=45&radius=20000&sort=distance&query="+keyword;
 
         Map<String, String> requestHeader = new HashMap<>();
-        requestHeader.put("X-Naver-Client-Id", id);
-        requestHeader.put("X-Naver-Client-Secret", pw);
+//        requestHeader.put("X-Naver-Client-Id", id);
+//        requestHeader.put("X-Naver-Client-Secret", pw);
+        requestHeader.put("Authorization", "KakaoAK "+key);
         String responseBody = get(webAddress, requestHeader);
 
-        log.debug(responseBody);
+        responseBody = idTracker(responseBody);
 
         return responseBody;
+    }
+
+    public String putMineOrRecommend(long placeId,long mine, long recommend){
+        Place place = findPlace(placeId);
+
+        if(place != null){
+            place.setMine(place.getMine()+mine);
+            place.setRecommend(place.getRecommend()+recommend);
+            placeRepository.save(place);
+            return "placeId : "+placeId+"\nmine : "+place.getMine()+"\nrecommend : "+place.getRecommend();
+        } else {
+            Place newPlace = new Place();
+            newPlace.setPlaceId(placeId);
+            newPlace.setMine(mine);
+            newPlace.setRecommend(recommend);
+            placeRepository.save(newPlace);
+            return "placeId : "+newPlace.getPlaceId()+"\nmine : "+newPlace.getMine()+"\nrecommend : "+newPlace.getRecommend();
+        }
     }
 
     private static String get(String webAddress, Map<String, String> requestHeader) {
@@ -74,7 +111,7 @@ public class PlaceService {
     }
 
     private static String readBody(InputStream body) throws UnsupportedEncodingException {
-        InputStreamReader streamReader = new InputStreamReader(body, "UTF-8");
+        InputStreamReader streamReader = new InputStreamReader(body,"UTF-8");
 
         try (BufferedReader lineReader = new BufferedReader(streamReader)) {
             StringBuilder responseBody = new StringBuilder();
@@ -82,14 +119,71 @@ public class PlaceService {
             String line;
             while ((line = lineReader.readLine()) != null) {
                 responseBody.append(line);
-                log.debug(line);
             }
-
-
 
             return responseBody.toString();
         } catch (IOException e) {
             throw new RuntimeException("API response reading failed.", e);
+        }
+    }
+
+    private String idTracker(String jsonData) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonData);
+
+            JsonNode placesNode = jsonNode.get("documents");
+
+            if(placesNode.isArray()){
+                for(JsonNode placeNode : placesNode){
+                    long id = placeNode.get("id").asLong();
+
+                    log.info("id : "+id);
+
+                    PairJ<Place, Boolean> place = findPlaceWithBoolean(id);
+
+                    ObjectNode objectNode = (ObjectNode) placeNode;
+                    if(place.getSecond()){
+                        objectNode.put("mine", place.getFirst().getMine());
+                        objectNode.put("recommend", place.getFirst().getRecommend());
+                    } else {
+                        objectNode.put("mine",0);
+                        objectNode.put("recommend", 0);
+                    }
+                    JsonNode changedNode = objectNode;
+                    placeNode = changedNode;
+                }
+            }
+            else {
+                log.warn("No 'places' array found in the JSON data.");
+            }
+
+            return objectMapper.writeValueAsString(jsonNode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private PairJ<Place, Boolean> findPlaceWithBoolean(long placeId){
+        Optional<Place> optionalPlace = placeRepository.findById(placeId);
+        PairJ<Place, Boolean> result = new PairJ<>();
+        result.setSecond(optionalPlace.isPresent());    // false면 없고 true면 있음
+        if(optionalPlace.isEmpty()){
+            result.setFirst(null);
+        } else {
+            result.setFirst(optionalPlace.orElseThrow());
+        }
+        return result;
+    }
+
+    private Place findPlace(long placeId){
+        Optional<Place> optionalPlace = placeRepository.findById(placeId);
+        if(optionalPlace.isEmpty()){
+            return null;
+        }else {
+            Place place = optionalPlace.orElseThrow();
+            return place;
         }
     }
 }
