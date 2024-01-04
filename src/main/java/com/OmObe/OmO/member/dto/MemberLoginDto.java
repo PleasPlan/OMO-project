5ebf872d37095347b0ee78f107f2de598e9f0c90
seunghyun711 <seunghyun711@gmail.com>
2024-01-05 03:20:07 +0900

    [be/fix] : MemberLoginDto 필드 수정(password에서 MemberRole로 변경)

--- src/main/java/com/OmObe/OmO/member/dto/MemberLoginDto.java
+++ src/main/java/com/OmObe/OmO/member/dto/MemberLoginDto.java
@@ -1,16 +1,19 @@
 package com.OmObe.OmO.member.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.OmObe.OmO.member.entity.Member;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MemberLoginDto { // 클라이언트의 인증 정보를 수신하는 loginDto
     @NotBlank
     private String email;
 
     @NotBlank
-    private String password;
+    private Member.MemberRole memberRole;
 }
