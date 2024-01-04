24a9385f5fb8b86b4844c80260b00bbd7d78f253
seunghyun711 <seunghyun711@gmail.com>
2024-01-05 03:42:44 +0900

    [be/fix] : SecurityContext에 Authentication 객체 저장하는 로직 수정

--- src/main/java/com/OmObe/OmO/auth/filter/JwtVerificationFilter.java
+++ src/main/java/com/OmObe/OmO/auth/filter/JwtVerificationFilter.java
@@ -1,17 +1,18 @@
 package com.OmObe.OmO.auth.filter;
 
 import com.OmObe.OmO.auth.jwt.JwtTokenizer;
-import com.OmObe.OmO.auth.utils.ErrorResponder;
 import com.OmObe.OmO.auth.utils.MemberAuthorityUtils;
 import com.OmObe.OmO.exception.BusinessLogicException;
 import com.OmObe.OmO.exception.ExceptionCode;
+import com.OmObe.OmO.member.dto.MemberLoginDto;
+import com.OmObe.OmO.member.entity.Member;
+import com.OmObe.OmO.member.repository.MemberRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,7 +25,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.SignatureException;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +35,7 @@ public class JwtVerificationFilter extends OncePerRequestFilter { // request 당
     private final JwtTokenizer jwtTokenizer;
     private final MemberAuthorityUtils authorityUtils;
     private final RedisTemplate redisTemplate;
+    private final MemberRepository memberRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -55,7 +56,6 @@ public class JwtVerificationFilter extends OncePerRequestFilter { // request 당
 
         try{
             Map<String, Object> claims = verifyJws(request); // jwt 검증
-            setAuthenticationToContext(claims); // Authentication 객체 SecurityContext에 저장
         } catch (ExpiredJwtException ee) {
             // 액세스 토큰이 만료된 경우
             log.info("catch ExpiredJwtException");
@@ -74,6 +74,21 @@ public class JwtVerificationFilter extends OncePerRequestFilter { // request 당
         } catch (Exception e) {
             request.setAttribute("exception", e);
         }
+
+        // access token으로 memberId 추출
+        Long memberId = getMemberIdFromAccessToken(accessToken);
+        // memberId를 이용해 찾은 Member 객체
+        Member findMember = memberRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        // SecurityContext에 등록할 Member 객체
+        MemberLoginDto memberLoginDto = MemberLoginDto.builder()
+                .email(findMember.getEmail())
+                .memberRole(findMember.getMemberRole())
+                .build();
+
+        // SecurityContext에 MemberLoginDto 객체 등록
+        setAuthenticationToContext(memberLoginDto);
 
         filterChain.doFilter(request, response); // 다음 필터 호출
     }
@@ -94,11 +109,22 @@ public class JwtVerificationFilter extends OncePerRequestFilter { // request 당
         return claims;
     }
 
+    // jws로 memberId 추출하는 메서드
+    private Long getMemberIdFromAccessToken(String accessToken) {
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        Map<String, Object> claims = jwtTokenizer.getClaims(accessToken, base64EncodedSecretKey).getBody();
+
+        log.info("memberId : {}", claims.get("memberId"));
+        return Long.parseLong(claims.get("memberId").toString());
+    }
+
     // Authentication 객체 SecurityContext에 저장하는 메서드
-    private void setAuthenticationToContext(Map<String, Object> claims) {
-        String username = (String) claims.get("username"); // username
-        List<GrantedAuthority> authorities = authorityUtils.createAuthorities((List) claims.get("roles")); // 권한 정보 기반으로 List<GrantedAuthority> 생성
-        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);// 위 정보를 포함한 Authentication 객체 생성
-        SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContext에 Authentication 객체 저장
+    private void setAuthenticationToContext(MemberLoginDto memberLoginDto) {
+        List<GrantedAuthority> authorities = authorityUtils.createAuthorities(memberLoginDto.getMemberRole()); // 권한 정보 기반으로 List<GrantedAuthority> 생성
+        // Authentication 객체 생성
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(memberLoginDto.getEmail(), null, authorities);
+        // SecurityContext에 Authentication 객체 저장
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
