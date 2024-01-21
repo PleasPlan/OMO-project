8fd04cb5812a7a5208341e236ac922857c9b5dfd
seunghyun711 <seunghyun711@gmail.com>
2024-01-21 22:01:31 +0900

    BoardReportMapper 생성

---
+++ src/main/java/com/OmObe/OmO/report/boardreport/mapper/BoardReportMapper.java
@@ -0,0 +1,27 @@
+package com.OmObe.OmO.report.boardreport.mapper;
+
+import com.OmObe.OmO.report.boardreport.dto.BoardReportDto;
+import com.OmObe.OmO.report.boardreport.entity.BoardReport;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BoardReportMapper {
+    // BoardReportDto.Post -> BoardReport
+    BoardReport boardReportPostDtoToBoardReport(BoardReportDto.Post post);
+
+    // Board -> BoardReportDto.Response
+    default BoardReportDto.Response boardReportToBoardResponseDto(BoardReport boardReport){
+        if (boardReport == null) {
+            return null;
+        } else {
+            BoardReportDto.Response response = new BoardReportDto.Response();
+            response.setBoardReportId(boardReport.getBoardReportId());
+            response.setReportTYpe(boardReport.getReportType());
+            response.setReason(boardReport.getReason());
+            return response;
+        }
+    }
+}
