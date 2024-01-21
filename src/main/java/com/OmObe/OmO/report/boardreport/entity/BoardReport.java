4bfb0ebd3ebb29493fb15fd985fcca9c2e3fde19
seunghyun711 <seunghyun711@gmail.com>
2024-01-21 19:54:16 +0900

    [be/feat] : BoardReport 엔티티 클래스 생성

---
+++ src/main/java/com/OmObe/OmO/report/boardreport/entity/BoardReport.java
@@ -0,0 +1,61 @@
+package com.OmObe.OmO.report.boardreport.entity;
+
+import com.OmObe.OmO.Board.entity.Board;
+import com.OmObe.OmO.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class BoardReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardReportId;
+
+    /*
+    <신고 유형>
+    1. 부적절한 주제
+    2. 부정확한 정보
+    3. 중복 게시물 도배
+    4. 주제와 맞지 않음
+    5. 욕설 및 비방
+    6. 기타(신고 사유 작성 필수
+     */
+    @Column(nullable = false)
+    private int reportType; // 신고 유형
+
+    @Column(nullable = true)
+    private String reason; // 신고 사유(신고 유형 6(기타)인 경우 필수 작성)
+
+    // BoardReport - Member 다대일 매핑
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    // BoardReport - Board 다대일 매핑
+    @ManyToOne
+    @JoinColumn(name = "BOARD_ID")
+    private Board board;
+
+    public void setReportType(int reportType) {
+        this.reportType = reportType;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+}
