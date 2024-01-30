caca6e75399888916066589ce96e3306ca0e04f4
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-01-30 19:22:10 +0900

    feat: MyCourse 엔티티 작성

---
+++ src/main/java/com/OmObe/OmO/MyCourse/MyCourse.java
@@ -0,0 +1,49 @@
+package com.OmObe.OmO.MyCourse;
+
+import com.OmObe.OmO.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class MyCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long courseId;
+
+    @Column
+    private String courseName;
+
+    @Column
+    private String placeName;
+
+    @Column
+    private long placeId;
+
+    @Column
+    private LocalDateTime times;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private Integer viewCount = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "WRITER")
+    private Member member;
+    
+    @OneToOne
+    private MyCourse nextCourse;
+}
