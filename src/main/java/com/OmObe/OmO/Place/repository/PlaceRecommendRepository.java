72a8a5782df79bfd79815cd87d1c94d818b1faff
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-02-14 20:17:23 +0900

    feat: pageUtility 제네릭 클래스 추가

--- src/main/java/com/OmObe/OmO/Place/repository/PlaceRecommendRepository.java
+++ src/main/java/com/OmObe/OmO/Place/repository/PlaceRecommendRepository.java
@@ -4,9 +4,10 @@ import com.OmObe.OmO.Place.entity.Place;
 import com.OmObe.OmO.Place.entity.PlaceRecommend;
 import com.OmObe.OmO.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface PlaceRecommendRepository extends JpaRepository<PlaceRecommend, Long> {
+public interface PlaceRecommendRepository extends JpaRepository<PlaceRecommend, Long>, JpaSpecificationExecutor<PlaceRecommend> {
     Optional<PlaceRecommend> findByMemberAndPlace(Member member, Place place);
 }
