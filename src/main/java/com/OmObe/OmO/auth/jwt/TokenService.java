95cd0a3c7f59ffe5961e1f22a68356e21d6dd029
seunghyun711 <seunghyun711@gmail.com>
2024-02-23 02:14:01 +0900

    [be/feat] : TokenService - 토큰의 유효기간 계산하는 메서드 추가

--- src/main/java/com/OmObe/OmO/auth/jwt/TokenService.java
+++ src/main/java/com/OmObe/OmO/auth/jwt/TokenService.java
@@ -1,6 +1,8 @@
 package com.OmObe.OmO.auth.jwt;
 
 import com.OmObe.OmO.member.entity.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,10 +31,15 @@ public class TokenService { // 토큰 생성 클래스
 
     public String delegateRefreshToken(Member member) { // refresh token 생성 메서드
         String subject = member.getEmail();
-        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
         String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
         String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
 
         return refreshToken;
+    }
+
+    // 남은 유효시간 계산 메서드
+    public Long calculateExpiration(Jws<Claims> claims){
+        return claims.getBody().getExpiration().getTime() - new Date().getTime();
     }
 }
