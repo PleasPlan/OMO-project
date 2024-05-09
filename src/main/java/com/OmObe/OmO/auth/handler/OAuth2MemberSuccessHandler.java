2e437cb894b1a1fcacb41e58c2600d2a4d7023dc
seunghyun711 <seunghyun711@gmail.com>
2024-05-09 17:07:22 +0900

    [be/fix] : oauth 로그인 후 리다이렉트 경로 수정

--- src/main/java/com/OmObe/OmO/auth/handler/OAuth2MemberSuccessHandler.java
+++ src/main/java/com/OmObe/OmO/auth/handler/OAuth2MemberSuccessHandler.java
@@ -111,6 +111,7 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 //                .host("api.oneulmohae.co.kr") // todo : 프론트엔드 배포 후 변경 예정
                 .port(5173)
 //                .port(8080)
+                .path("/LoginLoading")
                 .queryParams(queryParams)
                 .build()
                 .toUri();
